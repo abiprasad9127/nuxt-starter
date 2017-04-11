57fc53f51d0d740a09da7740db7b365f78ebe91c
abiprasad9127 <prasad.ab@husky.neu.edu>
2017-04-11 18:15:48 -0400

    img

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
   <section class="container">
-    <img src="https://www.google.com/search?q=netlify&tbm=isch&tbs=simg:CAQSlAEJa-DX9Yn5s-UaiAELEKjU2AQaAggJDAsQsIynCBphCl8IAxInwwG9AqEJwQOwA8ADqQM58Qa9Aaco7CGSKd0o0DaVKZMpojT9M5g-GjCs6YWb0cDADy0D3VVoVgaX-WDXhJYiJWX8BU2fSk-o4QIb0U9dpogYJnRbLMMB-mogBAwLEI6u_1ggaCgoICAESBLDdt64M&sa=X&ved=0ahUKEwjV586Mtp3TAhXp0FQKHeYHCGUQwg4IIigA&biw=1280&bih=542#imgrc=9tM6aCLAiR5mZM:" />
+    <img src="https://www.netlify.com/img/global/meta-image.jpg" />
     <h1 class="title">
       Netlify Assesment
     </h1>
