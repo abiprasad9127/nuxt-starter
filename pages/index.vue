a797e836c8b9de64c48bdb4ae80a6c87a1f914ca
abiprasad9127 <prasad.ab@husky.neu.edu>
2017-04-12 13:54:59 -0400

    check

--- pages/index.vue
+++ pages/index.vue
@@ -7,45 +7,32 @@
     <nuxt-link class="button" to="/about">
       Hello!
     </nuxt-link>
-    
-    <body>
+    <html lang="en">
+<head>
+  <title>Bootstrap Theme Simply Me</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+<body>
 
-<div class="container-fluid bg-1 text-center">
+<div class="container-fluid">
   <h3>Who Am I?</h3>
-  <img src="bird.jpg" class="img-circle" alt="Bird">
+  <img src="bird.jpg" alt="Bird">
   <h3>I'm an adventurer</h3>
 </div>
 
-<div class="container-fluid bg-2 text-center">
-  <h3>What Am I?</h3>
-  <p>Lorem ipsum..</p>
-</div>
-
-<div class="container-fluid bg-3 text-center">
-  <h3>Where To Find Me?</h3>
-  <p>Lorem ipsum..</p>
-</div>
-
 </body>
-
-
+</html>
+    
+   
 
 <style scoped>
 .title
 {
   margin: 50px 0;
-  <style>
-.bg-1 { 
-    background-color: #1abc9c; /* Green */
-    color: #ffffff;
-}
-.bg-2 { 
-    background-color: #474e5d; /* Dark Blue */
-    color: #ffffff;
-}
-.bg-3 { 
-    background-color: #ffffff; /* White */
-    color: #555555;
 }
 </style>
 
