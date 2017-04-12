fa336c1417ece238c02187f44977b3b098843259
abiprasad9127 <prasad.ab@husky.neu.edu>
2017-04-12 14:58:36 -0400

    add forms

--- pages/index.vue
+++ pages/index.vue
@@ -19,6 +19,25 @@
 }
 </style>
 
+<style>
+.bg-grey {
+    background-color: #f6f6f6;
+}
+</style>
+
+<div class="container-fluid">
+  <h2>About Company Page</h2>
+  <h4>Lorem ipsum..</h4> 
+  <p>Lorem ipsum..</p>
+  <button class="btn btn-default btn-lg">Get in Touch</button>
+</div>
+
+<div class="container-fluid bg-grey">
+  <h2>Our Values</h2>
+  <h4><strong>MISSION:</strong> Our mission lorem ipsum..</h4> 
+  <p><strong>VISION:</strong> Our vision Lorem ipsum..
+</div>
+
 
 <div class="jumbotron">
   <h1>Company</h1> 
