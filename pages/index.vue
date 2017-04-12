8e7440978fb953c495b4d8e8c39ba9a2da96fd4b
abiprasad9127 <prasad.ab@husky.neu.edu>
2017-04-12 17:08:01 -0400

    add format

--- pages/index.vue
+++ pages/index.vue
@@ -27,6 +27,24 @@
 }
 </style>
 
+<style>
+.bg-grey {
+    background-color: #f6f6f6;
+}
+</style>
+
+<style>
+.jumbotron {
+    background-color: #f4511e;
+    color: #fff;
+    padding: 100px 25px;
+}
+
+.container-fluid {
+    padding: 60px 50px;
+}
+</style>
+
 <nav class="navbar navbar-default navbar-fixed-top">
   <div class="container">
     <div class="navbar-header">
@@ -48,6 +66,19 @@
     </div>
   </div>
 </nav>
+
+<div class="container-fluid">
+  <h2>About Company Page</h2>
+  <h4>Lorem ipsum..</h4> 
+  <p>Lorem ipsum..</p>
+  <button class="btn btn-default btn-lg">Get in Touch</button>
+</div>
+
+<div class="container-fluid bg-grey">
+  <h2>Our Values</h2>
+  <h4><strong>MISSION:</strong> Our mission lorem ipsum..</h4> 
+  <p><strong>VISION:</strong> Our vision Lorem ipsum..
+</div>
 
 <div class="jumbotron">
   <h1>Company</h1> 
