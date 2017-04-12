57073f25ddeefcc852e165d420f2c5adf0fe7380
abiprasad9127 <prasad.ab@husky.neu.edu>
2017-04-12 14:55:10 -0400

    next

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,15 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <title>Bootstrap Theme Company</title>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-</head>
+<template>
+  <section class="container">
+    <img src="https://www.netlify.com/img/global/meta-image.jpg" />
+    <h1 class="title">
+      Netlify Assesment
+    </h1>
+    <nuxt-link class="button" to="/about">
+      Hello!
+    </nuxt-link>
+    
+    
+  </head>
 <body>
 
 <div class="jumbotron">
@@ -17,3 +19,17 @@
 
 </body>
 </html>
+</section>
+</template>
+    
+ 
+<style scoped>
+.title
+{
+  margin: 50px 0;
+}
+</style>
+
+
+
+
