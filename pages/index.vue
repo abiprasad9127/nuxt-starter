a9d26ebacf4a47753623462e76b8093dbe74eaaa
abiprasad9127 <prasad.ab@husky.neu.edu>
2017-04-12 16:33:23 -0400

    changes

--- pages/index.vue
+++ pages/index.vue
@@ -1,18 +1,14 @@
-<template>
-  <section class="container">
-    <img src="https://www.netlify.com/img/global/meta-image.jpg" />
-    <h1 class="title">
-      Netlify Assesment
-    </h1>
-    <nuxt-link class="button" to="/about">
-      Hello!
-    </nuxt-link>
-    
+
    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous"> 
   </head>
 <body>
 
-<div class="container">
+
+
+<template>
+  <section class="container">
+  
+  <div class="container">
   <h3>Tabs</h3>
   <ul class="nav nav-tabs">
     <li class="active"><a href="#">Home</a></li>
@@ -23,6 +19,16 @@
   <br>
   <p><strong>Note:</strong> This example shows how to create a basic navigation tab. It is not toggleable/dynamic yet (you can't click on the links to display different content)- see the last example in the Bootstrap Tabs and Pills Tutorial to find out how this can be done.</p>
 </div>
+
+    <img src="https://www.netlify.com/img/global/meta-image.jpg" />
+    <h1 class="title">
+      Netlify Assesment
+    </h1>
+    <nuxt-link class="button" to="/about">
+      Hello!
+    </nuxt-link>
+    
+
 
 <div class="jumbotron">
   <h1>Company</h1> 
