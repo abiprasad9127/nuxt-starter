a20bc47a2443abeebd9d791737a94a98f6e164ae
abiprasad9127 <prasad.ab@husky.neu.edu>
2017-04-12 12:25:49 -0400

    iframe again

--- pages/index.vue
+++ pages/index.vue
@@ -7,71 +7,14 @@
     <nuxt-link class="button" to="/about">
       Hello!
     </nuxt-link>
-  <html>
-<head>
-<style>
-div.container {
-    width: 100%;
-    border: 1px solid gray;
-}
-
-header, footer {
-    padding: 1em;
-    color: white;
-    background-color: black;
-    clear: left;
-    text-align: center;
-}
-
-nav {
-    float: left;
-    max-width: 160px;
-    margin: 0;
-    padding: 1em;
-}
-
-nav ul {
-    list-style-type: none;
-    padding: 0;
-}
-   
-nav ul a {
-    text-decoration: none;
-}
-
-article {
-    margin-left: 170px;
-    border-left: 1px solid gray;
-    padding: 1em;
-    overflow: hidden;
-}
-</style>
-</head>
+ <html>
 <body>
 
-<div class="container">
+<iframe height="300px" width="100%" src="demo_iframe.htm" name="iframe_a"></iframe>
 
-<header>
-   <h1>City Gallery</h1>
-</header>
-  
-<nav>
-  <ul>
-    <li><a href="#">London</a></li>
-    <li><a href="#">Paris</a></li>
-    <li><a href="#">Tokyo</a></li>
-  </ul>
-</nav>
+<p><a href="https://www.w3schools.com" target="iframe_a">W3Schools.com</a></p>
 
-<article>
-  <h1>London</h1>
-  <p>London is the capital city of England. It is the most populous city in the  United Kingdom, with a metropolitan area of over 13 million inhabitants.</p>
-  <p>Standing on the River Thames, London has been a major settlement for two millennia, its history going back to its founding by the Romans, who named it Londinium.</p>
-</article>
-
-<footer>Copyright &copy; W3Schools.com</footer>
-
-</div>
+<p>When the target of a link matches the name of an iframe, the link will open in the iframe.</p>
 
 </body>
 </html>
