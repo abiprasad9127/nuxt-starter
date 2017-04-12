f12931ec8422f352d755066c939f22a0ba11c8be
abiprasad9127 <prasad.ab@husky.neu.edu>
2017-04-12 16:41:27 -0400

    delete carasoul

--- pages/index.vue
+++ pages/index.vue
@@ -1,19 +1,18 @@
-
-   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous"> 
-  </head>
-<body>
-
 <template>
   <section class="container">
-  <img src="https://www.netlify.com/img/global/meta-image.jpg" />
+    <img src="https://www.netlify.com/img/global/meta-image.jpg" />
     <h1 class="title">
       Netlify Assesment
     </h1>
     <nuxt-link class="button" to="/about">
       Hello!
     </nuxt-link>
-  
-  <div class="container">
+    
+   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous"> 
+  </head>
+<body>
+
+<div class="container">
   <h3>Tabs</h3>
   <ul class="nav nav-tabs">
     <li class="active"><a href="#">Home</a></li>
@@ -22,45 +21,15 @@
     <li><a href="#">Menu 3</a></li>
   </ul>
   <br>
+  <p><strong>Note:</strong> This example shows how to create a basic navigation tab. It is not toggleable/dynamic yet (you can't click on the links to display different content)- see the last example in the Bootstrap Tabs and Pills Tutorial to find out how this can be done.</p>
 </div>
-    
+
 <div class="jumbotron">
   <h1>Company</h1> 
   <p>We specialize in blablabla</p> 
 </div>
 
-<h2>What our customers say</h2>
-<div id="myCarousel" class="carousel slide text-center" data-ride="carousel">
-  <!-- Indicators -->
-  <ol class="carousel-indicators">
-    <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-    <li data-target="#myCarousel" data-slide-to="1"></li>
-    <li data-target="#myCarousel" data-slide-to="2"></li>
-  </ol>
 
-  <!-- Wrapper for slides -->
-  <div class="carousel-inner" role="listbox">
-    <div class="item active">
-    <h4>"This company is the best. I am so happy with the result!"<br><span style="font-style:normal;">Michael Roe, Vice President, Comment Box</span></h4>
-    </div>
-    <div class="item">
-      <h4>"One word... WOW!!"<br><span style="font-style:normal;">John Doe, Salesman, Rep Inc</span></h4>
-    </div>
-    <div class="item">
-      <h4>"Could I... BE any more happy with this company?"<br><span style="font-style:normal;">Chandler Bing, Actor, FriendsAlot</span></h4>
-    </div>
-  </div>
-
-  <!-- Left and right controls -->
-  <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
-    <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-    <span class="sr-only">Previous</span>
-  </a>
-  <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
-    <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-    <span class="sr-only">Next</span>
-  </a>
-</div>
 
 </body>
 </html>
@@ -74,4 +43,3 @@
   margin: 50px 0;
 }
 </style>
-
