c4cacd3ffe4cc0342e776b99404484a0252dd9f8
abiprasad9127 <prasad.ab@husky.neu.edu>
2017-04-12 17:39:40 -0400

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,14 @@
 
-  
+  <template>
+<section class="container">
+    <img src="https://www.netlify.com/img/global/meta-image.jpg" />
+    <h1 class="title">
+      Netlify Assesment
+    </h1>
+    <nuxt-link class="button" to="/about">
+      Hello!
+    </nuxt-link>
+
     
   <head>
   <title>Bootstrap Theme Company Page</title>
@@ -13,15 +22,6 @@
   </head>
 <body>
 
-<template>
-<section class="container">
-    <img src="https://www.netlify.com/img/global/meta-image.jpg" />
-    <h1 class="title">
-      Netlify Assesment
-    </h1>
-    <nuxt-link class="button" to="/about">
-      Hello!
-    </nuxt-link>
 
 <style>
 .jumbotron { 
