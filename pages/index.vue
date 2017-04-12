0f1b1e1f3931e534c9d15cdad731ea74d18af104
abiprasad9127 <prasad.ab@husky.neu.edu>
2017-04-12 16:58:40 -0400

    old template

--- pages/index.vue
+++ pages/index.vue
@@ -1,4 +1,12 @@
-
+<template>
+  <section class="container">
+    <img src="https://www.netlify.com/img/global/meta-image.jpg" />
+    <h1 class="title">
+      Netlify Assesment
+    </h1>
+    <nuxt-link class="button" to="/about">
+      Hello!
+    </nuxt-link>
     
    <head>
   <title>Bootstrap Theme Company</title>
@@ -42,15 +50,7 @@
 </body>
 </html>
 
-<template>
-  <section class="container">
-    <img src="https://www.netlify.com/img/global/meta-image.jpg" />
-    <h1 class="title">
-      Netlify Assesment
-    </h1>
-    <nuxt-link class="button" to="/about">
-      Hello!
-    </nuxt-link>
+
 </section>
 </template>
     
