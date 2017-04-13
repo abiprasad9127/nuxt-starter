ab904576c4e40c62ac0e9e68a192dd6c1b11863f
abiprasad9127 <prasad.ab@husky.neu.edu>
2017-04-13 12:37:01 -0400

    script

--- pages/index.vue
+++ pages/index.vue
@@ -357,9 +357,16 @@ margin-left:20px;
     </a>
   </div>
 </div>
+      <script>
+$(document).ready(function(){
+  // Add smooth scrolling to all links in navbar + footer link
+  $(".navbar a, footer a[href='#myPage']").on('click', function(event) {
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
       // Store hash
       var hash = this.hash;
-
       // Using jQuery's animate() method to add smooth page scroll
       // The optional number (900) specifies the number of milliseconds it takes to scroll to the specified area
       $('html, body').animate({
@@ -375,7 +382,6 @@ margin-left:20px;
   $(window).scroll(function() {
     $(".slideanim").each(function(){
       var pos = $(this).offset().top;
-
       var winTop = $(window).scrollTop();
         if (pos < winTop + 600) {
           $(this).addClass("slide");
