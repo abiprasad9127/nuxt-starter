78c0e7792d0fd6b82eb16f10a3f9c7e2ce314a41
abiprasad9127 <prasad.ab@husky.neu.edu>
2017-04-13 03:32:48 -0400

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -116,7 +116,7 @@ margin-left:20px;
         <span class="icon-bar"></span>                        
       </button>
     </div>
-    <p color="white"><strong>Abinaya's Profile</strong></p>
+    <ul>Abinaya's Profile<</ul>
     <div class="collapse navbar-collapse" id="myNavbar">
       <ul class="nav navbar-nav navbar-right">
         <li><a href="#q1">Question1</a></li>
@@ -144,7 +144,7 @@ margin-left:20px;
 </div>
 
 <div class="fluid1">
-  <h3 text-align="center">Question 1</h3>
+  <h3>Question 1</h3>
   <h4>"Rank your 5 favorite, and least favorite, activities from this list: https://gist.github.com/fool/b0f254ff8c72a5765b6a9138249789d6"<br></h4> 
 </div>
 
