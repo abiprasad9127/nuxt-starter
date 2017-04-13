9beca50c56da5aeb39af65131a5b49a3d7e91357
abiprasad9127 <prasad.ab@husky.neu.edu>
2017-04-13 01:53:30 -0400

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -7,6 +7,7 @@
     </h1>
 
  <head>
+  <title>Bootstrap Theme Company Page</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
